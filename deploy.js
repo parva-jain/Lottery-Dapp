ac4b9836f6c92e25e19fd6572c239d30e22dd310
parva-jain <parvajainpjjp@gmail.com>
2021-11-09 10:03:18 +0530

    Deploying Contract in testnet

---
+++ deploy.js
@@ -0,0 +1,26 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+  'bone zone despair broccoli tennis tribe glare artist emotion furnace tree security',
+  // remember to change this to your own phrase!
+  'https://rinkeby.infura.io/v3/7afefe2c25f74ba29f3038676ae08a58'
+  // remember to change this to your own endpoint!
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log(interface)
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
+};
+deploy();
